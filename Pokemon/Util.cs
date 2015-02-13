7817bd00d92845cc2c00effc80931b466cd50794
odanado <odan3240@gmail.com>
2015-02-13 19:53:12 +0900

    Utilクラスを追加

---
+++ Pokemon/Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Pokemon
+{
+    class Util
+    {
+        public static string toId(string str)
+        {
+            str = str.Replace("é","e");
+            str = str.Replace("♂","M");
+            str = str.Replace("♀","F");
+
+            str = str.ToLower();
+            str = Regex.Replace(str,"[^a-z0-9]+","");
+
+            return str;
+        }
+    }
+}
