9e104fc495f6642a6885dfaa951d0cd1540ae7a4
odanado <odan3240@gmail.com>
2015-03-31 21:51:52 +0900

    Pokemonを追加した

--- Pokemon/BaseStats.cs
+++ Pokemon/BaseStats.cs
@@ -14,33 +14,6 @@ namespace Pokemon
         public uint spa { set; get; }
         public uint spd { set; get; }
         public uint spe { set; get; }
-        public IndividualValues ivs { set; get; }
-        public EffortValues evs { set; get; }
-        public uint level { set; get; }
-
-
-        public BaseStats()
-        {
-            this.ivs = new IndividualValues();
-            this.evs = new EffortValues();
-            this.level = 50;
-            calcStats();
-        }
-
-        public BaseStats(uint hp, uint atk, uint def, uint spa, uint spd, uint spe)
-        {
-            this.hp = hp;
-            this.atk = atk;
-            this.def = def;
-            this.spa = spa;
-            this.spd = spd;
-            this.spe = spe;
-        }
-
-        private void calcStats()
-        {
-
-        }
-
+                
     }
 }
---
+++ Pokemon/Pokemon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class Pokemon
+    {
+        public BaseStats baseStats { get; set; }
+        public EffortValues effortValues { get; set; }
+        public IndividualValues individualValues { get; set; }
+        public Abilities abilities { get; set; }
+        public List<string> types { get; set; }
+
+        public Nature nature { get; set; }
+
+        public uint level { get; set; }
+
+        public double heightm { get; set; }
+        public double weightkg { get; set; }
+
+        public string key { get; set; }
+
+        public Stats stats_ { get; set; }
+
+        public Pokemon(string name)
+        {
+            var pokedex = new Pokedex(name);
+
+            key = pokedex.key;
+
+            baseStats = pokedex.baseStats;
+            effortValues = new EffortValues();
+            individualValues = new IndividualValues();
+            abilities = pokedex.abilities;
+            types = pokedex.types;
+
+            nature = new Nature();
+
+            level = 50;
+
+            heightm = pokedex.heightm;
+            weightkg = pokedex.weightkg;
+
+            stats_ = new Stats(baseStats, effortValues, individualValues, level, nature);
+
+        }
+
+        public Stats stats
+        {
+            get
+            {
+                stats_ = new Stats(baseStats, effortValues, individualValues, level, nature);
+                return stats_;
+            }
+        }
+
+    }
+}
