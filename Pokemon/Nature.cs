044b17c824fb5f5afe8e8016b455c9a28bed9e32
odanado <odan3240@gmail.com>
2015-03-31 21:27:05 +0900

    性格を追加

---
+++ Pokemon/Nature.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class Nature
+    {
+        public string key { set; get; }
+
+        public Dictionary<string, double> multiplier { set; get; }
+
+        public Nature(string name)
+        {
+            key = Util.toKey(name);
+
+            var natures = Util.loadJson("nature.json");
+
+            if (!natures.IsDefined(key))
+            {
+                throw new KeyNotFoundException(key + " は性格に存在しません");
+            }
+
+            string plus = (string) natures[key]["plus"];
+            string minus = (string) natures[key]["minus"];
+
+            multiplier = new Dictionary<string, double>();
+
+            multiplier["atk"] = 1.0;
+            multiplier["def"] = 1.0;
+            multiplier["spa"] = 1.0;
+            multiplier["spd"] = 1.0;
+            multiplier["spe"] = 1.0;
+
+            multiplier[plus] = 1.1;
+            multiplier[minus] = 0.9;
+
+
+            
+        }
+    }
+}
