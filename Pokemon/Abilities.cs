86c7366acd1d0b95a211525b5f8deb30ecfdf973
odanado <odan3240@gmail.com>
2015-03-31 19:22:44 +0900

    特性を追加

---
+++ Pokemon/Abilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class Abilities
+    {
+        public string Hidden { get; set; }
+        public string First { get; set; }
+        public string Second { get; set; }
+    }
+}
