ddc3b182b004796966708ffb828303b7e6dbb8ce
odanado <odan3240@gmail.com>
2015-04-02 22:55:40 +0900

    状態を保持するクラスを追加

---
+++ Pokemon/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class State
+    {
+        public bool isReflect { set; get; }
+        public bool isLightScreen { set; get; }
+        public bool isDoubleDamage { set; get; }
+        public bool isSunnyDay { set; get; }
+        public bool isRainDance { set; get; }
+        public bool isSandStorm { set; get; }
+
+        public State()
+        {
+            isReflect = false;
+            isLightScreen = false;
+            isDoubleDamage = false;
+            isSunnyDay = false;
+            isRainDance = false;
+            isSandStorm = false;
+        }
+    }
+}
